d89aa25710e4620fd9c02087ad10f0dff1a6bd17
Richard Tan <chardos@gmail.com>
2015-02-12 23:28:02 +1100

    punch card: squashArray() working

--- d3js/punchcard/js/helpers.js
+++ d3js/punchcard/js/helpers.js
@@ -13,3 +13,41 @@ pc.convertDateToArray = function(date){
 pc.areArraysEqual = function(a1,a2) {
   return JSON.stringify(a1)==JSON.stringify(a2);
 }
+
+
+pc.squashArray = function(arr){
+  //put first value in
+  var squashedArr = [];
+  squashedArr.push([ arr[0][0], arr[0][1], 1 ]);
+  testArray.shift();
+
+  $.each(arr, function(i, v){
+    var count = 0;
+    $.each(squashedArr, function(si, sv){
+      console.log('v:' + v);
+      console.log('sv:' + sv);
+      if( v[0] == sv[0] && v[1] == sv[1] ){
+        sv[2]++;
+        count = 0;
+        return false;
+      }
+      else{
+        count++;
+        console.log('count up');
+      }
+    });
+    if ( count > 0){
+      squashedArr.push( [v[0], v[1], 1] );
+    }
+    console.log('c:' + count);
+    console.log(squashedArr.toString());
+    console.log('__________');
+  });
+  console.log(squashedArr);
+}
+
+
+
+
+var testArray = [[0,1],[0,1],[0,1],[2,0],[2,0],[3,0],[2,0],[2,0],[4,0],[1,2],[1,3],[1,2]];
+pc.squashArray(testArray);
