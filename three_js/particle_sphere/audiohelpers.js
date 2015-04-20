0e9d4532157fbfacc49010a93ae6bf80faacc849
Richard Tan <chardos@gmail.com>
2015-04-20 23:35:13 +1000

    particle sphere: added pause/play and bottombar

--- three_js/particle_sphere/audiohelpers.js
+++ three_js/particle_sphere/audiohelpers.js
@@ -1,5 +1,8 @@
 var gettrack = document.getElementById('gettrack');
 var textinput = document.getElementById('textinput');
+var pauseBtn = document.getElementById('pause');
+var playBtn = document.getElementById('play');
+
 gettrack.addEventListener("click", function(e){
   console.log(SC);
   track_url = textinput.value;
@@ -7,4 +10,12 @@ gettrack.addEventListener("click", function(e){
     console.log(track.id);
     audioElement.src = 'https://api.soundcloud.com/tracks/' + track.id + '/stream?client_id=74a591aaa5e9a7723d674ccf5fffe0bc'
   });
-}, false);
+});
+
+pause.addEventListener("click", function(e){
+  audioElement.pause();
+});
+
+play.addEventListener("click", function(e){
+  audioElement.play();
+});
