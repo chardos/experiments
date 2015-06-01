5864df6ac7b659604498c937d944acdd8c4d51e8
Richard Tan <chardos@gmail.com>
2015-06-02 00:33:31 +1000

    Fabric js - reset obj if not below the separator

--- fabricjs/js/script.js
+++ fabricjs/js/script.js
@@ -1,11 +1,11 @@
 
-var W = {}
+var F = {}
 
-W.vars = {
+F.vars = {
 	
 };
 
-W.createTable1 = function(canvas){
+F.createTable1 = function(canvas){
   var Table1 = new fabric.Rect({
     name: 'Table1',
     left: 50,
@@ -14,18 +14,22 @@ W.createTable1 = function(canvas){
     width: 20,
     height: 45,
   });
-  Table1.setControlsVisibility({mtr: false})
+  Table1.hasControls = false;
   canvas.add(Table1);
 }
+F.createSeparator = function(canvas){
+  var separator = new fabric.Rect({
+    left: 0,
+    top: 100,
+    fill: 'black',
+    width: 900,
+    height: 1,
+  });
+  separator.set('selectable', false);
+  canvas.add(separator);
+}
 
-W.init = function() {
-  // create a wrapper around native canvas element (with id="c")
-  var canvas = new fabric.Canvas('c');
-  canvas.selection = true;
-
-  W.createTable1(canvas);
-  
-
+F.initEvents = function(canvas) {
   //on click event
   canvas.on('mouse:down', function(options) {
     if (options.target) {
@@ -33,29 +37,53 @@ W.init = function() {
     }
   });
 
-  //on drag finish
+  //on modify
   canvas.on('object:modified', function(options) {
-    if (options.target && options.target.name == 'Table1') {
-      //change name
-      console.log('an object was modified!');
-      options.target.name = 'table1';
 
-      //enable rotation
-      options.target.setControlsVisibility({mtr: true})
-      W.createTable1(canvas);
+    thisObj = options.target;
+
+    if ( thisObj.top > 100 ){ // check dropped below separator
+      if (options.target && options.target.name == 'Table1') {
+        //change name
+        console.log('an object was modified!');
+        thisObj.name = 'table1';
+
+        //reenable rotation
+        //thisObj.setControlsVisibility({mtr: true})
+        thisObj.hasControls = true;
+
+        F.createTable1(canvas);
+      }
+    }
+    else{
+      //return to original position
+      thisObj.left = thisObj.originalState.left;
+      thisObj.top = thisObj.originalState.top;
+      thisObj.setCoords(); //redraw after modifying
+      console.log(thisObj);
     }
   });
+}
 
+F.init = function() {
+  // create a wrapper around native canvas element (with id="c")
+  var canvas = new fabric.Canvas('c');
+  canvas.selection = true;
+
+  F.createTable1(canvas);
+  F.createSeparator(canvas);
+  F.initEvents(canvas);
 }
 
 
 $(document).ready(function() {
 
-	W.init();
+	F.init();
 	
 });
 
 
 
 
-  //rect.set('selectable', true); // make object unselectable
+  //rect.set('selectable', true); // make object selectable
+  //Table1.setControlsVisibility({mtr: false}) //disable rotation
