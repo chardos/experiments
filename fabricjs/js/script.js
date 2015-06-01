ae5446bf3c4bead5e54cfed30fcd87b14ca6145d
Richard Tan <chardos@gmail.com>
2015-06-01 22:48:40 +1000

    fabric js - add rotate control on modify

--- fabricjs/js/script.js
+++ fabricjs/js/script.js
@@ -12,18 +12,45 @@ W.initializeDOMVars = function(){
 W.init = function() {
   // create a wrapper around native canvas element (with id="c")
   var canvas = new fabric.Canvas('c');
+  canvas.selection = true;
 
-  // create a rectangle object
-  var rect = new fabric.Rect({
-    left: 100,
-    top: 100,
-    fill: 'red',
+
+  var Table1 = new fabric.Rect({
+    name: 'Table1',
+    left: 50,
+    top: 50,
+    fill: 'green',
     width: 20,
-    height: 20
+    height: 45,
   });
+  Table1.setControlsVisibility({mtr: false})
+
 
   // "add" rectangle onto canvas
-  canvas.add(rect);
+  canvas.add(Table1);
+
+  //on click event
+  canvas.on('mouse:down', function(options) {
+    if (options.target) {
+      console.log('an object was clicked! ', options.target);
+      options.target.setControlsVisibility({mtr: false})
+    }
+  });
+
+  //on drag finish
+  canvas.on('object:modified', function(options) {
+    if (options.target && options.target.name == 'Table1') {
+      //change name
+      console.log('an object was modified!');
+      options.target.name = 'table1';
+
+      //enable rotation
+      options.target.setControlsVisibility({mtr: true})
+      //recreate original
+    }
+  });
+
+
 }
 	
 
@@ -33,3 +60,8 @@ $(document).ready(function() {
 	W.init();
 	
 });
+
+
+
+
+  //rect.set('selectable', true); // make object unselectable
