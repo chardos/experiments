e47d3a8d1133b1748915207f936b93bc4348459d
chardos <chardos@gmail.com>
2016-05-21 16:04:14 +1000

    seats

---
+++ seats/js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	var wheight = $(window).outerHeight();
+	var $wrap = $('.wrap');
+	$(document).on('mousemove', function(e){
+		var yPos = e.pageY/wheight;
+		var deg = 20 - (10*yPos);
+		$wrap.css('transform', `rotateX(${deg}deg)`)
+	})
+
+	$(document).on('click', '.seat', function(){
+		$(this).find('.seat-front').toggleClass('s-flipped');
+	})
+})
