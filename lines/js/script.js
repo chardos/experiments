580a3b589656749a7c1332c16598ccd6aa632955
chardos <chardos@gmail.com>
2016-07-20 23:42:42 +1000

    Directions working

--- lines/js/script.js
+++ lines/js/script.js
@@ -14,22 +14,40 @@ ctx.stroke();
 function Line(){
   var x = Math.ceil(Math.random() * windowWidth)
   var y = Math.ceil(Math.random() * innerHeight)
+	var direction = 'down';
 	var last;
-	var current = {x, y};
+	var currentPosition = {x, y};
 	var interval = setInterval(function(){
 		if(last){
 			ctx.beginPath();
 			ctx.moveTo(last.x, last.y);
-			ctx.lineTo(current.x, current.y);
+			ctx.lineTo(currentPosition.x, currentPosition.y);
 			ctx.stroke();
 			ctx.closePath();
 		}
-		last = $.extend({}, current);
-		current.x++;
+		last = $.extend({}, currentPosition);
+
+
+		currentPosition = move(currentPosition, direction);
 
 
 		//move the pointer 1px right
 	},16)
+}
+
+function move(pos, direction){
+	if(direction == 'up'){
+		return { x: pos.x, y: pos.y - 1};
+	}
+	else if(direction == 'right'){
+		return { x: pos.x + 1, y: pos.y };
+	}
+	else if( direction == 'down' ){
+		return { x: pos.x, y: pos.y + 1 };
+	}
+	else if( direction == 'left' ){
+		return { x: pos.x - 1, y: pos.y };
+	}
 }
 
 var line = new Line();
