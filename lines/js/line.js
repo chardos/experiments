597280e9b750213864070cfa235a8eb7e803ded9
chardos <chardos@gmail.com>
2016-07-30 19:33:35 +1000

    Moved step function outside

--- lines/js/line.js
+++ lines/js/line.js
@@ -16,44 +16,10 @@ Linework.prototype.drawTo = function (x, y){
     this.setup();
     this.requiresSetup = false;
   }
-  var self = this;
 
-  //move one step forward
-  function step(){
-    // check if the current pos has reached (or gone past) the destination
-    if(self.hasReachedDestination(self.direction, self.nextPos, self.destination)){
-
-      //draw the line segment to the destination (instead of the nextPos,
-      //so it doesn't go past the destination).
-      self.drawLineSegment(ctx, self.currPos, self.destination);
-
-      self.queue.shift(1);
-      if(self.queue.length){
-        self.origin = self.destination;
-        self.destination = self.queue[0].position;
-        self.setup();
-      }
-      else{
-        //Queue has been completed.
-        self.origin = self.destination;
-        self.isAnimating = false;
-        self.requiresSetup = true;
-        return;
-      }
-    }
-    else{
-      self.drawLineSegment(ctx, self.currPos, self.nextPos);
-      //set next coordinates
-      self.currPos =  $.extend({}, self.nextPos);
-      self.nextPos = self.getNextPos(self.angle);
-    }
-
-    requestAnimationFrame(step)
-
-  }
   if(!this.isAnimating){
     // setInterval(step, 700)
-    step()
+    this.step(this)
     this.isAnimating = true;
   }
 
